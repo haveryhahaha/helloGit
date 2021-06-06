5629a097caa29d8eb8e2784cbc2b3d30288b1bb3
havery <havery_wjh@163.com>
2021-06-06 13:21:33 +0800

    第一次添加文件

---
+++ GitTest/src/main/java/com/atguigu/Git/helloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.Git;
+
+/**
+ * @author Havery
+ * @create 2021-06-06-13:12
+ */
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
