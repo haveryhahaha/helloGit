97a8a71d27e78b32dce359ec5d7686a1879bfb77
havery <havery_wjh@163.com>
2021-06-06 14:54:00 +0800

    创建分支

--- GitTest/src/main/java/com/atguigu/Git/helloGit.java
+++ GitTest/src/main/java/com/atguigu/Git/helloGit.java
@@ -8,6 +8,7 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("第一次更新");
+        System.out.println("创建分支");
 
     }
 }
